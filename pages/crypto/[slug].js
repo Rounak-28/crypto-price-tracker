a19865dbc0883fca7c20d1b1dea14a317f1f3ec0
Rounak-28 <95576871+Rounak-28@users.noreply.github.com>
2022-10-18 18:54:24 +0530

    fix

--- pages/crypto/[slug].js
+++ pages/crypto/[slug].js
@@ -1,5 +1,4 @@
 import { useRouter } from "next/router";
-import Head from "next/head";
 import { useEffect, useState } from "react";
 import Chart from "chart.js/auto";
 import { Line } from "react-chartjs-2";
@@ -25,42 +24,35 @@ const Slug = () => {
       .then((response) => response.json())
       .then((response) => setHistoricalData(response))
       .catch((err) => console.error(err));
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   console.log(historicalData);
   return (
-    <>
-    <Head>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    </Head>
-      <div className="w-[100vw] h-[100vh]">
-        <Line
-          data={{
-            labels: historicalData?.data?.history.map((item) => {
-              let date = new Date(item.timestamp * 1000);
-              // let time = `${date.getHours()}:${date.getMinutes()}`
-              return date.toLocaleDateString();
-            }),
+    <div className="w-[100vw] h-[100vh]">
+      <Line
+        data={{
+          labels: historicalData?.data?.history.map((item) => {
+            let date = new Date(item.timestamp * 1000);
+            // let time = `${date.getHours()}:${date.getMinutes()}`
+            return date.toLocaleDateString();
+          }),
 
-            datasets: [
-              {
-                data: historicalData?.data?.history.map((item) => item.price),
-                label: `Price in USD`,
-                borderColor: "#eebc1d",
-              },
-            ],
-          }}
-          options={{
-            elements: {
-              point: {
-                radius: 1,
-              },
+          datasets: [
+            {
+              data: historicalData?.data?.history.map((item) => item.price),
+              label: `Price in USD`,
+              borderColor: "#eebc1d",
             },
-          }}
-        />
-      </div>
-    </>
+          ],
+        }}
+        options={{
+          elements: {
+            point: {
+              radius: 1,
+            },
+          },
+        }}
+      />
+    </div>
   );
 };
 
